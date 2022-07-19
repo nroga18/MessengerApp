a8ab054d74117d0426fc27dde7e9ca5087c7c362
giorgi-urumashvili <gurum18@freeuni.edu.ge>
2022-07-19 20:37:58 +0400

    minor fix

--- app/src/main/java/ge/nrogava/messengerapp/util/ToastExtension.kt
+++ app/src/main/java/ge/nrogava/messengerapp/util/ToastExtension.kt
@@ -5,4 +5,5 @@ import android.widget.Toast
 
 fun Context.toast(text : String) {
     Toast.makeText(this,text, Toast.LENGTH_LONG).show()
+    //toast duration speed up would be better.
 }
--- app/src/main/java/ge/nrogava/messengerapp/views/ChatsViewModel.kt
+++ app/src/main/java/ge/nrogava/messengerapp/views/ChatsViewModel.kt
@@ -22,5 +22,4 @@ class ChatsViewModel : ViewModel() {
         rep.searchChats(_chatsLiveData,person)
     }
 
-
 }
