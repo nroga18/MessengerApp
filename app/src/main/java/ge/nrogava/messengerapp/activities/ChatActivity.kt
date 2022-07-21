db7afe85c5deb44af655e8e70c3b3b9c4ff08ba7
nroga18 <nroga18@freeuni.edu.ge>
2022-07-21 21:47:53 +0400

    add sign out functionality and remember signed in user

---
+++ app/src/main/java/ge/nrogava/messengerapp/activities/ChatActivity.kt
@@ -0,0 +1,12 @@
+package ge.nrogava.messengerapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ge.nrogava.messengerapp.R
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
